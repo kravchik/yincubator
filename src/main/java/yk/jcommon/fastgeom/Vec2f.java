332d534ce565ad75838669437bc91db78485c760
Yuri Kravchik <kravchiky@gmail.com>
2023-12-17 17:56:00 +0200

    * additional methods

--- src/main/java/yk/jcommon/fastgeom/Vec2f.java
+++ src/main/java/yk/jcommon/fastgeom/Vec2f.java
@@ -9,6 +9,7 @@ import yk.jcommon.utils.BadException;
 import yk.jcommon.utils.MyMath;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import static yk.jcommon.fastgeom.Vec3f.v3;
 import static yk.jcommon.utils.Util.sqr;
@@ -18,7 +19,7 @@ import static yk.jcommon.utils.Util.sqr;
  *
  * @author Yuri Kravchik Created 04.01.2009
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({"SuspiciousNameCombination"})
 public final class Vec2f implements Serializable {
     public float x, y;
 
@@ -26,195 +27,87 @@ public final class Vec2f implements Serializable {
     public static final Vec2f AXIS_X = new Vec2f(1, 0);
     public static final Vec2f AXIS_Y = new Vec2f(0, 1);
 
+    //constructors
     public Vec2f() {}
+    public static Vec2f v2(final float x, final float y) {return new Vec2f(x, y);}
+    public Vec2f(final float x, final float y) {this.x = x;this.y = y;}
+    public Vec2f(final Vec2f v) {this.x = v.x;this.y = v.y;}
+    public static Vec2f fromAngle(float angle) {return new Vec2f((float)Math.cos(angle), (float) Math.sin(angle));}
+    public Vec2f copy() {return new Vec2f(x, y);}
+    public void copyFrom(Vec2f other) {this.x = other.x;this.y = other.y;}
 
-    public static Vec2f v2(final float x, final float y) {
-        return new Vec2f(x, y);
-    }
+    //setters
+    public Vec3f v3WithX(float x) {return v3(x, this.x, y);}
+    public Vec3f v3WithY(float y) {return v3(x, y, this.y);}
+    public Vec3f v3WithZ(float z) {return v3(x, y, z);}
+    public Vec2f withX(float x) {return new Vec2f(x, y);}
+    public Vec2f withY(float y) {return new Vec2f(x, y);}
 
-    public Vec2f(final float x, final float y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Vec2f(final Vec2f v) {
-        this.x = v.x;
-        this.y = v.y;
-    }
-
-    public float length() {
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    public float lengthSqr() {
-        return x * x + y * y;
-    }
-
-    public float maxAbs() {
-        return Math.max(Math.abs(x), Math.abs(y));
-    }
-
-    public float minAbs() {
-        return Math.min(Math.abs(x), Math.abs(y));
-    }
-
-    public float scalarProduct(final Vec2f b) {
-        return x * b.x + y * b.y;
-    }
-
-    public float dot(final Vec2f b) {
-        return x * b.x + y * b.y;
-    }
-
-    @Override
-    public String toString() {
-        return "x: " + x + " y: " + y;
-    }
-
-    public Vec2f normalized() {
-        return normalized(1);
-    }
-
+    //distance
+    public float length() {return (float) Math.sqrt(x * x + y * y);}
+    public float lengthSqr() {return x * x + y * y;}
+    public Vec2f normalized() {return normalized(1);}
+    public float distanceSquared(Vec2f other) {return sqr(other.x - x) + sqr(other.y - y);}
     public Vec2f normalized(float len) {
         float m = len / length();
         if (Float.isNaN(m)) BadException.die("NaN for " + this);
         if (Float.isInfinite(m)) BadException.die("Infinite for " + this);
         return new Vec2f(x * m, y * m);
     }
+    public float dist(Vec2f b) {return sub(b).length();}
 
-    public Vec2f complexDiv(Vec2f by) {
+    //math
+    public float scalarProduct(final Vec2f b) {return x * b.x + y * b.y;}
+    public float dot(final Vec2f b) {return x * b.x + y * b.y;}
+    public Vec2f rot90() {return new Vec2f(-y, x);}
+    public Vec2f rot_90() {return new Vec2f(y, -x);}
+    public float cross(Vec2f other) {return x * other.y - y * other.x;}
+    public Vec2f cDiv(Vec2f by) {
         float len = 1 / by.length();
-        return new Vec2f(x * by.x + y * by.y, -x * by.y + y * by.x).mul(len);
+        return new Vec2f((x * by.x + y * by.y) * len, (-x * by.y + y * by.x) * len);
     }
-
-    public Vec2f complexDiv1(Vec2f by) {
-        return new Vec2f(x * by.x + y * by.y, -x * by.y + y * by.x);
-    }
-
-    public Vec2f complexMul(Vec2f by) {
-        return new Vec2f(x * by.x - y * by.y, x * by.y + y * by.x);
-    }
-
-    public Vec2f rot90() {
-        return new Vec2f(-y, x);
-    }
-
-    public Vec2f rot_90() {
-        return new Vec2f(y, -x);
-    }
-
-    public float cross  (Vec2f other) {
-        return x * other.y - y * other.x;
-    }
-
-    public float distanceSquared(Vec2f other) {
-        return sqr(other.x - x) + sqr(other.y - y);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Vec2f vec2f = (Vec2f) o;
-
-        if (Float.compare(vec2f.x, x) != 0) return false;
-        if (Float.compare(vec2f.y, y) != 0) return false;
-
-        return true;
-    }
-
-    public Vec2f cDivFast(Vec2f unit) {
-        return new Vec2f(x*unit.x+y*unit.y, -x*unit.y+y*unit.x);
-    }
-
-    public Vec2f cMul(Vec2f other) {
-        return new Vec2f(x*other.x-y*other.y, x*other.y+y*other.x);
-    }
-
-    public float getAngle() {
-        return (float) (Math.atan2(y, x));
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
-        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
-        return result;
-    }
-
-    public float dis(Vec2f pos) {
-        return (float) Math.sqrt(distanceSquared(pos));
-    }
-
-    public static Vec2f fromAngle(float angle) {
-        return new Vec2f((float)Math.cos(angle), (float) Math.sin(angle));
-    }
-
-    public boolean isBehind(Vec2f b) {
-        return this.scalarProduct(b) < 0;
-    }
-
-    public Vec2f negative() {
-        return new Vec2f(-x, -y);
-    }
-
-    public Vec2f copy() {
-        return new Vec2f(x, y);
-    }
-
-    public void copyFrom(Vec2f other) {
-        this.x = other.x;
-        this.y = other.y;
-    }
-
-    public float dist(Vec2f b) {
-        return sub(b).length();
-    }
-
-    public Vec3f v3WithX(float x) {
-        return v3(x, this.x, y);
-    }
-
-    public Vec3f v3WithY(float y) {
-        return v3(x, y, this.y);
-    }
-
-    public Vec3f v3WithZ(float z) {
-        return v3(x, y, z);
-    }
-
-    public Vec2f withSetX(float x) {
-        return new Vec2f(x, y);
-    }
-
-    public Vec2f withSetY(float y) {
-        return new Vec2f(x, y);
-    }
+    public Vec2f cDiv1(Vec2f unit) {return new Vec2f(x*unit.x+y*unit.y, -x*unit.y+y*unit.x);}
+    public Vec2f cMul(Vec2f other) {return new Vec2f(x*other.x-y*other.y, x*other.y+y*other.x);}
+    public float getAngle() {return (float) (Math.atan2(y, x));}
+    public boolean isBehind(Vec2f b) {return this.scalarProduct(b) < 0;}
+    public Vec2f negative() {return new Vec2f(-x, -y);}
 
 //2022 auto generated text
-/*2022*/public Vec2f add(Vec2f other) {return new Vec2f(x + other.x, y + other.y);}
-/*2022*/public Vec2f add(float v) {return new Vec2f(x + v, y + v);}
+/*2022*///operators
+/*2022*/public Vec2f add(Vec2f b) {return new Vec2f(x + b.x, y + b.y);}
+/*2022*/public Vec2f add(float b) {return new Vec2f(x + b, y + b);}
 /*2022*/public Vec2f add(float x, float y) {return new Vec2f(this.x + x, this.y + y);}
-/*2022*/public Vec2f sub(Vec2f other) {return new Vec2f(x - other.x, y - other.y);}
-/*2022*/public Vec2f sub(float v) {return new Vec2f(x - v, y - v);}
+/*2022*/public Vec2f sub(Vec2f b) {return new Vec2f(x - b.x, y - b.y);}
+/*2022*/public Vec2f sub(float b) {return new Vec2f(x - b, y - b);}
 /*2022*/public Vec2f sub(float x, float y) {return new Vec2f(this.x - x, this.y - y);}
-/*2022*/public Vec2f mul(Vec2f other) {return new Vec2f(x * other.x, y * other.y);}
-/*2022*/public Vec2f mul(float v) {return new Vec2f(x * v, y * v);}
+/*2022*/public Vec2f mul(Vec2f b) {return new Vec2f(x * b.x, y * b.y);}
+/*2022*/public Vec2f mul(float b) {return new Vec2f(x * b, y * b);}
 /*2022*/public Vec2f mul(float x, float y) {return new Vec2f(this.x * x, this.y * y);}
-/*2022*/public Vec2f div(Vec2f other) {return new Vec2f(x / other.x, y / other.y);}
-/*2022*/public Vec2f div(float v) {return new Vec2f(x / v, y / v);}
+/*2022*/public Vec2f div(Vec2f b) {return new Vec2f(x / b.x, y / b.y);}
+/*2022*/public Vec2f div(float b) {return new Vec2f(x / b, y / b);}
 /*2022*/public Vec2f div(float x, float y) {return new Vec2f(this.x / x, this.y / y);}
+/*2022*///0 args functions
+/*2022*/public Vec2f round() {return new Vec2f(Math.round(x), Math.round(y));}
 /*2022*/public Vec2f floor() {return new Vec2f(MyMath.floorFast(x), MyMath.floorFast(y));}
+/*2022*/public Vec2f ceil() {return new Vec2f(MyMath.ceil(x), MyMath.ceil(y));}
+/*2022*/public Vec2f abs() {return new Vec2f(MyMath.abs(x), MyMath.abs(y));}
+/*2022*///reducers
+/*2022*/public float sum() {return x + y;}
+/*2022*/public float product() {return x * y;}
+/*2022*/public float min() {return MyMath.min(x, y);}
+/*2022*/public float max() {return MyMath.max(x, y);}
+/*2022*/public float maxAbs() {return Math.max(Math.abs(x), Math.abs(y));}
+/*2022*/public float minAbs() {return Math.min(Math.abs(x), Math.abs(y));}
+/*2022*///binary functions
 /*2022*/public Vec2f min(Vec2f b) {return new Vec2f(MyMath.min(x, b.x), MyMath.min(y, b.y));}
 /*2022*/public Vec2f min(float b) {return new Vec2f(MyMath.min(x, b), MyMath.min(y, b));}
-/*2022*/public Vec2f min(float x, float y) {return new Vec2f(MyMath.min(this.x, x), MyMath.min(this.y, y));}
+/*2022*/public Vec2f min(float x, float y) {return new Vec2f(MyMath.min(x, x), MyMath.min(y, y));}
 /*2022*/public Vec2f max(Vec2f b) {return new Vec2f(MyMath.max(x, b.x), MyMath.max(y, b.y));}
 /*2022*/public Vec2f max(float b) {return new Vec2f(MyMath.max(x, b), MyMath.max(y, b));}
-/*2022*/public Vec2f max(float x, float y) {return new Vec2f(MyMath.max(this.x, x), MyMath.max(this.y, y));}
+/*2022*/public Vec2f max(float x, float y) {return new Vec2f(MyMath.max(x, x), MyMath.max(y, y));}
 /*2022*/public Vec2f cycle(Vec2f b) {return new Vec2f(MyMath.cycle(x, b.x), MyMath.cycle(y, b.y));}
 /*2022*/public Vec2f cycle(float b) {return new Vec2f(MyMath.cycle(x, b), MyMath.cycle(y, b));}
-/*2022*/public Vec2f cycle(float x, float y) {return new Vec2f(MyMath.cycle(this.x, x), MyMath.cycle(this.y, y));}
+/*2022*/public Vec2f cycle(float x, float y) {return new Vec2f(MyMath.cycle(x, x), MyMath.cycle(y, y));}
 //2022 auto generated text
 
 //gglsl auto generated text
@@ -245,9 +138,7 @@ public Vec2f pow(Vec2f power) {return Vec2f.v2((float)Math.pow(this.x, power.x),
 public Vec2f exp() {return Vec2f.v2((float)Math.exp(this.x), (float)Math.exp(this.y));}
 public Vec2f log() {return Vec2f.v2((float)Math.log(this.x), (float)Math.log(this.y));}
 public Vec2f sqrt() {return Vec2f.v2((float)Math.sqrt(this.x), (float)Math.sqrt(this.y));}
-public Vec2f abs() {return Vec2f.v2(Math.abs(this.x), Math.abs(this.y));}
 public Vec2f sign() {return Vec2f.v2(Math.signum(this.x), Math.signum(this.y));}
-public Vec2f ceil() {return Vec2f.v2((float)Math.ceil(this.x), (float)Math.ceil(this.y));}
 public Vec2f fract() {return Vec2f.v2(this.x - (float)Math.floor(this.x), this.y - (float)Math.floor(this.y));}
 public Vec2f mod(Vec2f by) {return Vec2f.v2((float)(this.x-by.x*Math.floor(this.x/by.x)), (float)(this.y-by.y*Math.floor(this.y/by.y)));}
 public Vec2f clamp(Vec2f min, Vec2f max) {return Vec2f.v2(Math.max(min.x, Math.min(this.x, max.x)), Math.max(min.y, Math.min(this.y, max.y)));}
@@ -257,4 +148,22 @@ public Vec2f mix(Vec2f to, float progress) {return Vec2f.v2(this.x * (1 - progre
 public Vec2f step(Vec2f value) {return Vec2f.v2(value.x < this.x ? 0 : 1, value.y < this.y ? 0 : 1);}
 public Vec2f smoothstep(Vec2f to, Vec2f progress) {return Vec2f.v2(progress.x < this.x ? 0 : progress.x > to.x ? 1 : progress.x*progress.x*(3 - 2*progress.x), progress.y < this.y ? 0 : progress.y > to.y ? 1 : progress.y*progress.y*(3 - 2*progress.y));}
 //gglsl auto generated text
+
+    @Override
+    public String toString() {
+        return "x: " + x + " y: " + y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vec2f vec2f = (Vec2f) o;
+        return Float.compare(x, vec2f.x) == 0 && Float.compare(y, vec2f.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
--- src/main/java/yk/jcommon/fastgeom/Vec2i.java
+++ src/main/java/yk/jcommon/fastgeom/Vec2i.java
@@ -1,147 +1,99 @@
 package yk.jcommon.fastgeom;
 
 import yk.jcommon.utils.MyMath;
-import yk.jcommon.utils.Reflector;
-import yk.ycollections.YList;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
 * User: Yuri Kravchik
 * Date: 30.08.2010
 * Time: 18:32:56
 */
+@SuppressWarnings("SuspiciousNameCombination")
 public final class Vec2i implements Serializable {
     public int x, y;
 
-    public static Vec2i ZERO = new Vec2i(0, 0);
+    public static final Vec2i ZERO = new Vec2i(0, 0);
     public static final Vec2i AXIS_X = new Vec2i(1, 0);
     public static final Vec2i AXIS_Y = new Vec2i(0, 1);
 
-    public Vec2i() {
-        x = 0;
-        y = 0;
-    }
+    //constructors
+    public Vec2i() {x = 0;y = 0;}
+    public static Vec2i v2i(int x, int y) {return new Vec2i(x, y);}
+    public Vec2i(int x, int y) {this.x = x;this.y = y;}
+    public Vec2i(Vec2i other) {x = other.x;y = other.y;}
+    public Vec2f toVec2f() {return new Vec2f(x, y);}
 
-    public static Vec2i v2i(int x, int y) {
-        return new Vec2i(x, y);
-    }
+    //distance
+    public float disSquared(Vec2i b) {return MyMath.sqr(x - b.x) + MyMath.sqr(y - b.y);}
+    public float dis(Vec2i b) {return (float) Math.sqrt(disSquared(b));}
+    public float len() {return MyMath.sqr(x) + MyMath.sqr(y);}
 
-    public Vec2i(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Vec2i(Vec2i other) {
-        x = other.x;
-        y = other.y;
-    }
-
-    public float disSquared(Vec2i b) {
-        int dx = x - b.x;
-        int dy = y - b.y;
-        return dx * dx + dy * dy;
-    }
-
-    public float dis(Vec2i b) {
-        return (float) Math.sqrt(disSquared(b));
-    }
-
-    public Vec2i addX(int x) {
-        return new Vec2i(this.x + x, y);
-    }
-    public Vec2i addY(int y) {
-        return new Vec2i(x, this.y + y);
-    }
-    public boolean is(int x, int y) {
-        return this.x == x && this.y == y;
-    }
-
-    @Override
-    public String toString() {
-        return x + ":" + y;
-    }
-
-    public Vec2i mul(float i) {
-        return new Vec2i((int)(x * i), (int)(y * i));
-    }
-
-    public Vec2i mul(float ox, float oy) {
-        return new Vec2i((int)(x * ox), (int)(y * oy));
-    }
-
-    public Vec2i div(float i) {
-        return new Vec2i((int)(x / i), (int)(y / i));
-    }
-
-    public Vec2i rnd(int radius) {
-        return new Vec2i(x + (int) (-radius + Math.random() * radius * 2),
-                y + (int) (-radius + Math.random() * radius * 2));
-    }
-
-    public Vec2f toVec2f() {
-        return new Vec2f(x, y);
-    }
-
-    public float len() {
-        return dis(ZERO);
-    }
-
+    //math
     public float angle() {
         double len = len();
         double angle = Math.acos((double)x/len);
         if (y < 0) angle = 2 * Math.PI - angle;
         return (float) angle;
     }
+    public Vec2i cMul(Vec2i other) {return v2i(x*other.x-y*other.y, x*other.y+y*other.x);}
+    public Vec2i rot90() {return new Vec2i(-y, x);}
+    public Vec2i rot_90() {return new Vec2i(y, -x);}
+    public Vec2i negative() {return new Vec2i(-x, -y);}
 
-    public void init(YList elements) {
-        //TODO remove
-        Reflector.set(this, "x", ((Number)elements.get(0)).intValue());
-        Reflector.set(this, "y", ((Number)elements.get(1)).intValue());
+    //2022 auto generated text
+/*2022*///operators
+/*2022*/public Vec2i add(Vec2i b) {return new Vec2i(x + b.x, y + b.y);}
+/*2022*/public Vec2i add(int b) {return new Vec2i(x + b, y + b);}
+/*2022*/public Vec2i add(int x, int y) {return new Vec2i(this.x + x, this.y + y);}
+/*2022*/public Vec2i sub(Vec2i b) {return new Vec2i(x - b.x, y - b.y);}
+/*2022*/public Vec2i sub(int b) {return new Vec2i(x - b, y - b);}
+/*2022*/public Vec2i sub(int x, int y) {return new Vec2i(this.x - x, this.y - y);}
+/*2022*/public Vec2i mul(Vec2i b) {return new Vec2i(x * b.x, y * b.y);}
+/*2022*/public Vec2i mul(int b) {return new Vec2i(x * b, y * b);}
+/*2022*/public Vec2i mul(int x, int y) {return new Vec2i(this.x * x, this.y * y);}
+/*2022*/public Vec2i div(Vec2i b) {return new Vec2i(x / b.x, y / b.y);}
+/*2022*/public Vec2i div(int b) {return new Vec2i(x / b, y / b);}
+/*2022*/public Vec2i div(int x, int y) {return new Vec2i(this.x / x, this.y / y);}
+/*2022*///0 args functions
+/*2022*/public Vec2i abs() {return new Vec2i(MyMath.abs(x), MyMath.abs(y));}
+/*2022*///reducers
+/*2022*/public int sum() {return x + y;}
+/*2022*/public int product() {return x * y;}
+/*2022*/public int min() {return MyMath.min(x, y);}
+/*2022*/public int max() {return MyMath.max(x, y);}
+/*2022*/public int maxAbs() {return Math.max(Math.abs(x), Math.abs(y));}
+/*2022*/public int minAbs() {return Math.min(Math.abs(x), Math.abs(y));}
+/*2022*///binary functions
+/*2022*/public Vec2i min(Vec2i b) {return new Vec2i(MyMath.min(x, b.x), MyMath.min(y, b.y));}
+/*2022*/public Vec2i min(int b) {return new Vec2i(MyMath.min(x, b), MyMath.min(y, b));}
+/*2022*/public Vec2i min(int x, int y) {return new Vec2i(MyMath.min(x, x), MyMath.min(y, y));}
+/*2022*/public Vec2i max(Vec2i b) {return new Vec2i(MyMath.max(x, b.x), MyMath.max(y, b.y));}
+/*2022*/public Vec2i max(int b) {return new Vec2i(MyMath.max(x, b), MyMath.max(y, b));}
+/*2022*/public Vec2i max(int x, int y) {return new Vec2i(MyMath.max(x, x), MyMath.max(y, y));}
+/*2022*/public Vec2i cycle(Vec2i b) {return new Vec2i(MyMath.cycle(x, b.x), MyMath.cycle(y, b.y));}
+/*2022*/public Vec2i cycle(int b) {return new Vec2i(MyMath.cycle(x, b), MyMath.cycle(y, b));}
+/*2022*/public Vec2i cycle(int x, int y) {return new Vec2i(MyMath.cycle(x, x), MyMath.cycle(y, y));}
+//2022 auto generated text
+
+    @Override
+    public String toString() {return x + ":" + y;}
+
+    public boolean equals(int x, int y) {
+        return this.x == x && this.y == y;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Vec2i vec2i = (Vec2i) o;
-
-        if (x != vec2i.x) return false;
-        if (y != vec2i.y) return false;
-
-        return true;
+        return x == vec2i.x && y == vec2i.y;
     }
 
     @Override
     public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
+        return Objects.hash(x, y);
     }
-
-//2022 auto generated text
-/*2022*/public Vec2i add(Vec2i other) {return new Vec2i(x + other.x, y + other.y);}
-/*2022*/public Vec2i add(int v) {return new Vec2i(x + v, y + v);}
-/*2022*/public Vec2i add(int x, int y) {return new Vec2i(this.x + x, this.y + y);}
-/*2022*/public Vec2i sub(Vec2i other) {return new Vec2i(x - other.x, y - other.y);}
-/*2022*/public Vec2i sub(int v) {return new Vec2i(x - v, y - v);}
-/*2022*/public Vec2i sub(int x, int y) {return new Vec2i(this.x - x, this.y - y);}
-/*2022*/public Vec2i mul(Vec2i other) {return new Vec2i(x * other.x, y * other.y);}
-/*2022*/public Vec2i mul(int v) {return new Vec2i(x * v, y * v);}
-/*2022*/public Vec2i mul(int x, int y) {return new Vec2i(this.x * x, this.y * y);}
-/*2022*/public Vec2i div(Vec2i other) {return new Vec2i(x / other.x, y / other.y);}
-/*2022*/public Vec2i div(int v) {return new Vec2i(x / v, y / v);}
-/*2022*/public Vec2i div(int x, int y) {return new Vec2i(this.x / x, this.y / y);}
-/*2022*/public Vec2i min(Vec2i b) {return new Vec2i(MyMath.min(x, b.x), MyMath.min(y, b.y));}
-/*2022*/public Vec2i min(int b) {return new Vec2i(MyMath.min(x, b), MyMath.min(y, b));}
-/*2022*/public Vec2i min(int x, int y) {return new Vec2i(MyMath.min(this.x, x), MyMath.min(this.y, y));}
-/*2022*/public Vec2i max(Vec2i b) {return new Vec2i(MyMath.max(x, b.x), MyMath.max(y, b.y));}
-/*2022*/public Vec2i max(int b) {return new Vec2i(MyMath.max(x, b), MyMath.max(y, b));}
-/*2022*/public Vec2i max(int x, int y) {return new Vec2i(MyMath.max(this.x, x), MyMath.max(this.y, y));}
-/*2022*/public Vec2i cycle(Vec2i b) {return new Vec2i(MyMath.cycle(x, b.x), MyMath.cycle(y, b.y));}
-/*2022*/public Vec2i cycle(int b) {return new Vec2i(MyMath.cycle(x, b), MyMath.cycle(y, b));}
-/*2022*/public Vec2i cycle(int x, int y) {return new Vec2i(MyMath.cycle(this.x, x), MyMath.cycle(this.y, y));}
-//2022 auto generated text
-
 }
