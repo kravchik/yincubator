b1475ef4928a47d694c6c11c20c54c4e9bcd8c0f
Yuri Kravchik <kravchiky@gmail.com>
2024-01-05 14:36:05 +0200

    * equals, toString

--- src/main/java/yk/jcommon/utils/Ptr.java
+++ src/main/java/yk/jcommon/utils/Ptr.java
@@ -1,5 +1,7 @@
 package yk.jcommon.utils;
 
+import java.util.Objects;
+
 /**
  * Like a pointer in CPP.
  * Can be used instead of T[] in lambdas.
@@ -13,5 +15,23 @@ public class Ptr<T> {
 
     public Ptr(T value) {
         this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Ptr{value=" + value + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ptr<?> ptr = (Ptr<?>) o;
+        return Objects.equals(value, ptr.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
